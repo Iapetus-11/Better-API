816ab06a16c027353b3118750e803af9ccca475c
Iapetus-11 <olimdahacker@gmail.com>
2020-07-27 17:40:09 -0400

    add  misc.js endpoint

---
+++ routes/misc.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/captcha', (req, res) => { // captcha generator, takes a length param
+  let length = 4;
+
+  if (req.query.length != null) {
+    length = parseInt(req.query.length);
+  }
+
+  if (length > 10 || length < 1) {
+    res.status(406).json({success: false, message: 'The length field should be an integer between 1 and 10.'})
+    return;
+  }
+
+  captcha = svgCaptcha.create(size=length); // returns json {"text": "text", "data": "svgshit"}
+  captcha.success = true;
+
+  res.json(captcha);
+});
+
+router.get('/gen/uuid', (req, res) => { // generates a uuid4
+  res.json({success: true, uuid: uuid.v4()});
+});
+
+module.exports = router;
