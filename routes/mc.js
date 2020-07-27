0dc8e9f36927352621c02de9d05fb9d8b1212379
Iapetus-11 <olimdahacker@gmail.com>
2020-07-27 19:13:02 -0400

    finish mc.js

--- routes/mc.js
+++ routes/mc.js
@@ -1,10 +1,23 @@
 const express = require('express');
-const http = require('http')
+const bent = require('bent')
 
+const getJSON = bent('json')
 const router = express.Router();
 
 router.get('/server', (req, res) => { // checks the status of a minecraft server, takes query params host and port
+  let host = req.query.host;
+  let port = req.query.port;
 
+  if (host == null){
+    res.status(406).json({success: false, message: 'host is a required field. (example: GET /mc/server?host=mc.hypixel.net&port=25565)'});
+    return;
+  }
+
+  if (port == null){
+    port = 25565;
+  }
+
+  res.json(await getJSON(`http://localhost:6942/mcping?host=${host}&port=${port}`)); // passes to local python aiohttp server running
 });
 
 module.exports = router;
