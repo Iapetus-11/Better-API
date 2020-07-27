a0baa86032a080bb326277c0383c385431cf33d6
Iapetus-11 <olimdahacker@gmail.com>
2020-07-27 18:06:28 -0400

    progress

---
+++ routes/mc.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+function bedrock_edition
+
+router.get('/server', (req, res) => { // checks the status of a minecraft server
+
+})
+
+module.exports = router;
