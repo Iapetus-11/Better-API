5f58431cacfc6ef81b79af650b419a560ca36a53
Iapetus-11 <olimdahacker@gmail.com>
2020-07-28 01:56:20 -0400

    add /bulkcaptcha endpoint

--- routes/gen.js
+++ routes/gen.js
@@ -4,23 +4,43 @@ const uuid = require('uuid');
 
 const router = express.Router();
 
-router.get('/captcha', (req, res) => { // captcha generator, takes a length param
-  let length = parseInt(req.query.length);
+router.get('/captcha', (req, res) => { // captcha generator, takes a size param
+  let size = parseInt(req.query.size);
 
-  if (length == null || length == NaN){
-    length = 4;
+  if (size == null || size == NaN){
+    size = 4;
   }
 
-  if (length > 10 || length < 1) {
-    res.status(400).json({success: false, message: 'The length field must be an integer between 1 and 10.'});
+  if (size > 10 || size < 1) {
+    res.status(400).json({success: false, message: 'The size field must be an integer between 1 and 10.'});
     return;
   }
 
-  captcha = svgCaptcha.create(size=length); // returns json {"text": "text", "data": "svgshit"}
+  captcha = svgCaptcha.create(size=size); // returns json {"text": "text", "data": "svgshit"}
   captcha.success = true;
 
   res.json(captcha);
 });
+
+router.get('/bulkcaptcha', (req, res) => {
+  let size = parseInt(req.query.size);
+  let amount = parseInt(req.query.amount);
+
+  if (size == null || size == NaN) {
+    size = 4;
+  }
+
+  if (amount == null || amount == NaN) {
+    amount = 50;
+  }
+
+  captchas = [];
+  for (i = 0; i < amount; i++) {
+    captchas.put(svgCaptcha.create(size=size));
+  }
+
+  res.json({success: true, captchas: captchas});
+})
 
 router.get('/uuid', (req, res) => { // generates a uuid4
   res.json({success: true, uuid: uuid.v4()});
