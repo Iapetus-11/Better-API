48eedd2417005c4f2cae1a44de0b19cb7c27e738
Iapetus-11 <olimdahacker@gmail.com>
2020-07-27 21:01:41 -0400

    make sure port is between 0-65535

--- routes/mc.js
+++ routes/mc.js
@@ -5,15 +5,20 @@ const router = express.Router();
 
 router.get('/mcping', (req, res) => { // checks the status of a minecraft server, takes query params host and port
   let host = req.query.host;
-  let port = req.query.port;
+  let port = parseInt(req.query.port);
 
-  if (host == null){
+  if (host == null) {
     res.status(406).json({success: false, message: 'host is a required field. (example: GET /mc/mcping?host=mc.hypixel.net&port=25565)'});
     return;
   }
 
-  if (port == null){
+  if (port == null) {
     port = 0;
+  }
+
+  if (port > 65535 || port < 0) {
+    res.status(406).json({success: false, message: 'the port field must be an integer between 0 and 65535.'});
+    return;
   }
 
   axios.get('http://localhost:6942/mcping', {headers: {'host': host, 'port': port}})
