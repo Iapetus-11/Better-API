87e6a004444569d4f06274ad335c895028b8c787
Iapetus-11 <olimdahacker@gmail.com>
2020-07-27 20:01:29 -0400

    change /server endp to /mcping

--- routes/mc.js
+++ routes/mc.js
@@ -4,12 +4,12 @@ const axios = require('axios')
 const getJSON = bent('json')
 const router = express.Router();
 
-router.get('/server', (req, res) => { // checks the status of a minecraft server, takes query params host and port
+router.get('/mcping', (req, res) => { // checks the status of a minecraft server, takes query params host and port
   let host = req.query.host;
   let port = req.query.port;
 
   if (host == null){
-    res.status(406).json({success: false, message: 'host is a required field. (example: GET /mc/server?host=mc.hypixel.net&port=25565)'});
+    res.status(406).json({success: false, message: 'host is a required field. (example: GET /mc/mcping?host=mc.hypixel.net&port=25565)'});
     return;
   }
 
