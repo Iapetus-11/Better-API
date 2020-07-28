1d44582ecd040fc358ed638d16874f1166ea84c5
Iapetus-11 <olimdahacker@gmail.com>
2020-07-28 16:12:04 -0400

    use /color route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 80;
 
 app.use('/gen', require('./routes/gen'));
 app.use('/mc', require('./routes/mc'));
+app.use('/color', require('./routes/color'));
 
 // actually run the server
 app.listen(port, () => {
