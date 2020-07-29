e4b283c92d1d3d749316767acbba28da58dd0955
Iapetus-11 <olimdahacker@gmail.com>
2020-07-29 13:26:41 -0400

    fix py example for color image endp

--- examples/python3.6+/color_image_endpoint.py
+++ examples/python3.6+/color_image_endpoint.py
@@ -1,10 +1,12 @@
+# This example fetches an image from the /color/image endpoint and saves it as a file
+
 import requests
-from binascii import a2b_base64
+import urllib
 
 
 r = requests.get('http://localhost/color/image?color=77d5af&x=200&y=200')  # fetch the image
 
 with open('color.png', 'wb+') as image_file:  # open up the image we're going to create
-    data_url = r.text.split("\"")[1] # takes the data url from the returned html
-    binary_data = a2b_base64(data_url)  # turns the base64 / data url into binary
-    image_file.write(binary_data)  # writes the binary data to the file
+    data_url = r.text.split("\"")[0] # gets the data url from the returned html (kinda jank)
+    data_url_file = urllib.request.urlopen(data_url)  # turns the data url into a file
+    image_file.write(data_url_file.file.read())  # writes the data to the file
