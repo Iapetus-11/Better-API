f85c610dc3ac973e217b396d2351688028da6746
Iapetus-11 <olimdahacker@gmail.com>
2020-07-29 13:01:44 -0400

    no need for tmp dir

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ app.use('/gen', require('./routes/gen')); // generation endponts
 app.use('/mc', require('./routes/mc')); // mc utils endpoints
 app.use('/color', require('./routes/color')); // color endpoints
 
-app.use('/img', express.static('img')); // server static dir /img
-
 // actually run the server
 app.listen(port, () => {
     console.log(`theapi running on port ${port}`);
