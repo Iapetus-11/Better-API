78defb2c94eeb51cb3122078ea848fe44c2e765b
Iapetus-11 <olimdahacker@gmail.com>
2020-07-29 13:27:32 -0400

    fix py example for color image endpoint

--- examples/python3.6+/color_image_endpoint.py
+++ examples/python3.6+/color_image_endpoint.py
@@ -7,6 +7,6 @@ import urllib
 r = requests.get('http://localhost/color/image?color=77d5af&x=200&y=200')  # fetch the image
 
 with open('color.png', 'wb+') as image_file:  # open up the image we're going to create
-    data_url = r.text.split("\"")[0] # gets the data url from the returned html (kinda jank)
+    data_url = r.text.split("\"")[1] # gets the data url from the returned html (kinda jank)
     data_url_file = urllib.request.urlopen(data_url)  # turns the data url into a file
     image_file.write(data_url_file.file.read())  # writes the data to the file
