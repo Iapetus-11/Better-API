a84ec4160a8f8772973e7ab3f904f5c453e6c2b5
Iapetus-11 <olimdahacker@gmail.com>
2020-07-29 01:05:37 -0400

    remove image after done with sending it

--- routes/color.js
+++ routes/color.js
@@ -192,6 +192,8 @@ router.get('/image', (req, res) => {
 
   res.sendFile(`${constants.baseDir}/tmp/img/${color}_${x}x${y}.png`);
 
+  fs.unlinkSync(`./tmp/img/${color}_${x}x${y}.png`);
+
   // res.redirect(`${constants.baseUrl}/img/${color}_${x}x${y}.png`);
 
   // let img = genColorImage(color, x, y);
