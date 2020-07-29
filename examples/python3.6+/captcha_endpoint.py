3c17f8ebe0ea260b8af294bd1a0a67462211e4a8
Iapetus-11 <olimdahacker@gmail.com>
2020-07-29 13:30:37 -0400

    less confuzzling code

--- examples/python3.6+/captcha_endpoint.py
+++ examples/python3.6+/captcha_endpoint.py
@@ -1,3 +1,17 @@
+# This example fetches a captcha from the endpoint and writes the captcha image to a file
+
+import requests
+import urllib
+
+
+r = requests.get('http://localhost/gen/captcha?size=4&imgonly=false')  # fetch data from the captcha endpoint
+
+with open('captcha.png', 'wb+') as image_file:  # open up the image we're going to create
+    data_url = r.json()['data']  # gets the returned json and gets the data url from that
+    data_url_file = urllib.request.urlopen(data_url)  # turns the data url into a file
+    image_file.write(data_url_file.file.read())  # writes the data to the file
+
+# ignore the below
 # old code for when the captcha endpoint returned svgs instead of data urls for the image
 """
 import requests
@@ -15,16 +29,3 @@ with open('test.svg', 'w+') as f:
 drawing = svg2rlg('test.svg')
 renderPM.drawToFile(drawing, 'test.png', fmt='PNG')
 """
-
-# This example fetches a captcha from the endpoint and writes the captcha image to a file
-
-import requests
-import urllib
-
-
-r = requests.get('http://localhost/gen/captcha?size=4&imgonly=false')  # fetch data from the captcha endpoint
-
-with open('captcha.png', 'wb+') as image_file:  # open up the image we're going to create
-    data_url = r.json()['data']  # gets the returned json and gets the data url from that
-    data_url_file = urllib.request.urlopen(data_url)  # turns the data url into a file
-    image_file.write(data_url_file.file.read())  # writes the data to the file
