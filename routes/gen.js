c7e6e68889118e7f8e449a0f9dc1a672505ac986
Iapetus-11 <olimdahacker@gmail.com>
2020-07-29 01:39:24 -0400

    use different package for captchas

--- routes/gen.js
+++ routes/gen.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const svgCaptcha = require('svg-captcha');
+const captcha = require('@haileybot/captcha-generator');
 const uuid = require('uuid');
 
 const router = express.Router();
@@ -21,36 +21,6 @@ router.get('/captcha', (req, res) => { // captcha generator, takes a size param
 
   res.json(captcha);
 });
-
-router.get('/bulkcaptcha', (req, res) => { // bulk genereates up to 100 captchas at a time, takes size and amount params
-  let size = parseInt(req.query.size);
-  let amount = parseInt(req.query.amount);
-
-  if (size == null || size == NaN) {
-    size = 4;
-  }
-
-  if (size > 10 || size < 1) {
-    res.status(400).json({success: false, message: 'The size field must be an integer between 1 and 10.'});
-    return;
-  }
-
-  if (amount == null || amount == NaN) {
-    amount = 50;
-  }
-
-  if (amount > 75 || amount < 1) {
-    res.status(400).json({success: false, message: 'The amount field must be an integer between 1 and 75.'});
-    return;
-  }
-
-  captchas = [];
-  for (i = 0; i < amount; i++) {
-    captchas.push(svgCaptcha.create({size: size, noise: 3, color: true}));
-  }
-
-  res.json({success: true, captchas: captchas});
-})
 
 router.get('/uuid', (req, res) => { // generates a uuid4
   res.json({success: true, uuid: uuid.v4()});
