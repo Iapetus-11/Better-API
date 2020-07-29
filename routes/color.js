4caac50a7e87f18f9fb9d9f28fe36c8a865c90fa
Iapetus-11 <olimdahacker@gmail.com>
2020-07-28 21:09:19 -0400

    require() fs

--- routes/color.js
+++ routes/color.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const canvas = require('canvas');
+const fs = require('fs');
 
 const router = express.Router();
 const validHex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
