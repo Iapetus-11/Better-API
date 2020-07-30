e14c92cc3a172c50d1760719ce21891a3cbbdb5b
Iapetus-11 <olimdahacker@gmail.com>
2020-07-30 15:40:16 -0400

    update example

--- examples/python3/color_image_endpoint.py
+++ examples/python3/color_image_endpoint.py
@@ -4,9 +4,9 @@ import requests
 import urllib
 
 
-r = requests.get('http://localhost/color/image?color=77d5af&x=200&y=200')  # fetch the image
+r = requests.get('http://localhost/color/image?color=77aff0&x=200&y=200')  # fetch data from the captcha endpoint
 
 with open('color.png', 'wb+') as image_file:  # open up the image we're going to create
-    data_url = r.text.split("\"")[1]  # gets the data url from the returned html (kinda jank)
+    data_url = r.json()['data']  # gets the returned json and gets the data url from that
     data_url_file = urllib.request.urlopen(data_url)  # turns the data url into a file
     image_file.write(data_url_file.file.read())  # writes the data to the file
