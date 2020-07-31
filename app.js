d0f09be4e60be7864ca3c3544c8252106fb329a0
Iapetus-11 <olimdahacker@gmail.com>
2020-07-30 20:47:45 -0400

    Use Constants' appPort

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ const Constants = require('./constants');
 
 const app = Express();
 app.use(Helmet())
-const port = 80;
 
 app.use('/gen', require('./routes/gen')); // generation endponts
 app.use('/mc', require('./routes/mc')); // mc utils endpoints
 app.use('/color', require('./routes/color')); // color endpoints
 
 // actually run the server
-app.listen(port, () => {
-    console.log(`theapi running on port ${port}`);
+app.listen(Constants.appPort, () => {
+    console.log(`theapi running on port ${Constants.appPort}`);
 });
