57b9ef5299734106207ce92d0edb4d70deb982ad
Iapetus-11 <olimdahacker@gmail.com>
2020-07-31 18:32:18 -0400

    drawMOTDPlain() method

--- routes/mc.js
+++ routes/mc.js
@@ -25,22 +25,7 @@ async function drawFavicon(ctx, faviData) {
   }
 }
 
-async function renderServerImage(host, port) {
-  let image = Canvas.createCanvas(768, 140);
-  let ctx = image.getContext('2d');
-
-  ctx.imageSmoothingEnabled = false;
-  ctx.quality = 'nearest'; // nearest cause dealing with pixels cause Minecraft ya
-
-  let bgImage = await Canvas.loadImage('assets/mcserver_background.png');
-  ctx.drawImage(bgImage, 0, 0, 768, 140);
-
-  let statusData = await pingMCServer(host, port);
-
-  drawFavicon(ctx, statusData.favicon)
-  .then(() => {})
-
-  let motd = statusData.description;
+async function drawMOTD(ctx, motd) {
   let motdFinal = '';
 
   try { // motd has a chance to be a weird dict / array or regular text
@@ -60,6 +45,56 @@ async function renderServerImage(host, port) {
   ctx.textBaseline = 'bottom'; // set bottom of text to bottom of image
   ctx.fillStyle = "#222"
   ctx.fillText(motdFinal, 140/*padding of image 6+end of image*/+6/*extra padding*/, 140/*height of image*/-22/*font px size*/-24/*extra padding*/);
+}
+
+async function drawMOTDPlain(ctx, motd) {
+  let motdFinal = '';
+
+  try { // motd has a chance to be a weird dict / array or regular text
+    for (i = 0; i < motd.extra.length; i++) {
+      motdFinal = motdFinal.concat(motd.extra[i].text);
+    }
+    motdFinal = motdFinal.concat(motd.text);
+  } catch (err) {
+    motdFinal = '';
+    for (i = 0; i < motd.length; i++) {
+      if (motd.charAt(i) == '§') {
+        i++;
+      } else {
+        motdFinal.concat(motd.charAt(i));
+      }
+    }
+  }
+
+  while (motdFinal == void(0)) {} // for some reason this needs to be here
+
+  // Server motd / desc
+  ctx.font = '22px "Minecraft"'; // monotype font, 15px wide, 3px between letters @ 22 px font || .measureText()
+  ctx.textAlign = 'start';
+  ctx.textBaseline = 'bottom'; // set bottom of text to bottom of image
+  ctx.fillStyle = "#222"
+  ctx.fillText(motdFinal, 140/*padding of image 6+end of image*/+6/*extra padding*/, 140/*height of image*/-22/*font px size*/-24/*extra padding*/);
+
+  return true;
+}
+
+async function renderServerImage(host, port) {
+  let image = Canvas.createCanvas(768, 140);
+  let ctx = image.getContext('2d');
+
+  ctx.imageSmoothingEnabled = false;
+  ctx.quality = 'nearest'; // nearest cause dealing with pixels cause Minecraft ya
+
+  let bgImage = await Canvas.loadImage('assets/mcserver_background.png');
+  ctx.drawImage(bgImage, 0, 0, 768, 140);
+
+  let statusData = await pingMCServer(host, port); // "blocking" ping the mc server
+
+  drawFavicon(ctx, statusData.favicon) // draw favicon to image
+  .then(() => {});
+
+  drawMOTDPlain(ctx, statusData.description) // draw a plain white motd
+  .then(() => {});
 
   ctx.font = '22px "Minecraft"';
   ctx.textAlign = 'start';
