7520a3349375700a25f4328fe4f067342051f6e7
Iapetus-11 <olimdahacker@gmail.com>
2020-07-31 22:22:39 -0400

    Apparently I cannot spell today.

--- routes/mc.js
+++ routes/mc.js
@@ -207,7 +207,7 @@ router.get('/mcpingimg', RateLimit({windowMs: 2500, max: 1}) /*every 2.5 sec*/, 
     if (imgOnly != 'true') {
       res.json({success: true, data: image.toDataURL()});
     } else {
-      res.send(image.toDataUrl());
+      res.send(image.toDatURL());
     }
   })
   .catch(e => {
