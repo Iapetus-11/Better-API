fcb54c842e9874f08f50dfed52119a22f4eb4d6b
Iapetus-11 <olimdahacker@gmail.com>
2020-07-31 21:27:49 -0400

    fix colors in constants.js

--- constants.js
+++ constants.js
@@ -3,22 +3,22 @@ constants = {
   baseUrl: 'http://localhost',
   ipsToIgnore: ['localhost', '0.0.0.0', '192.168.', '127.0.0.1'],
   minecraftColors: [
-    ['Dark Red', '4', 'AA0000'],
-    ['Red', 'c', 'FF5555'],
-    ['Gold', '6', 'FFAA00'],
-    ['Yellow', 'e', 'FFFF55'],
-    ['Dark Green', '2', '00AA00'],
-    ['Green', 'a', '55FF55'],
-    ['Aqua', 'b', '55FFFF'],
-    ['Dark Aqua', '3', '00AAAA'],
-    ['Dark Blue', '1', '0000AA'],
-    ['Blue', '9', '5555FF'],
-    ['Light Purple', 'd', 'FF55FF'],
-    ['Dark Purple', '5', 'AA00AA'],
-    ['White', 'f', 'FFFFFF'],
-    ['Gray', '7', 'AAAAAA'],
-    ['Dark Gray', '8', '555555'],
-    ['Black', '0', '000000']
+    ['dark_red', '4', 'AA0000'],
+    ['red', 'c', 'FF5555'],
+    ['gold', '6', 'FFAA00'],
+    ['yellow', 'e', 'FFFF55'],
+    ['dark_green', '2', '00AA00'],
+    ['green', 'a', '55FF55'],
+    ['aqua', 'b', '55FFFF'],
+    ['dark_aqua', '3', '00AAAA'],
+    ['dark_blue', '1', '0000AA'],
+    ['blue', '9', '5555FF'],
+    ['light_purple', 'd', 'FF55FF'],
+    ['dark_purple', '5', 'AA00AA'],
+    ['white', 'f', 'FFFFFF'],
+    ['gray', '7', 'AAAAAA'],
+    ['dark_gray', '8', '555555'],
+    ['black', '0', '000000']
   ]
 }
 
