4d21e3964c7e69603f60765383c6d38b72a1c8e3
Iapetus-11 <olimdahacker@gmail.com>
2020-07-31 21:22:19 -0400

    add list of Minecraft colors

--- constants.js
+++ constants.js
@@ -1,7 +1,25 @@
 constants = {
   appPort: 80,
   baseUrl: 'http://localhost',
-  ipsToIgnore: ['localhost', '0.0.0.0', '192.168.', '127.0.0.1']
+  ipsToIgnore: ['localhost', '0.0.0.0', '192.168.', '127.0.0.1'],
+  minecraftColors: [
+    ['Dark Red', '4', 'AA0000'],
+    ['Red', 'c', 'FF5555'],
+    ['Gold', '6', 'FFAA00'],
+    ['Yellow', 'e', 'FFFF55'],
+    ['Dark Green', '2', '00AA00'],
+    ['Green', 'a', '55FF55'],
+    ['Aqua', 'b', '55FFFF'],
+    ['Dark Aqua', '3', '00AAAA'],
+    ['Dark Blue', '1', '0000AA'],
+    ['Blue', '9', '5555FF'],
+    ['Light Purple', 'd', 'FF55FF'],
+    ['Dark Purple', '5', 'AA00AA'],
+    ['White', 'f', 'FFFFFF'],
+    ['Gray', '7', 'AAAAAA'],
+    ['Dark Gray', '8', '555555'],
+    ['Black', '0', '000000']
+  ]
 }
 
 module.exports = constants;
