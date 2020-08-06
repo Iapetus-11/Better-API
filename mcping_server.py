701641242e91173d3253a010599ef1d57b7526da
Iapetus-11 <olimdahacker@gmail.com>
2020-08-05 21:04:26 -0400

    debug

--- mcping_server.py
+++ mcping_server.py
@@ -145,11 +145,12 @@ def raknet_status(ip, port):
         'method': 'raknet'
     }
     s_dict['motd'] = data[1]
-    
+
     try:
         s_dict['map'] = data[7]
         s_dict['gamemode'] = data[8]
     except IndexError:
+        print(data)
         pass
 
     return s_dict
